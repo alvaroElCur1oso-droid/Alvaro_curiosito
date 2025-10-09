256a9a3598e9ab9d63b4ab3e711f8fc0b446d224
alvaroElCur1oso-droid <alvaroagular741@gmail.com>
2025-10-08 19:00:06 -0500

    Add files via upload

---
+++ javape.js
@@ -0,0 +1,11 @@
+function mensaje() {
+    alert(`🍲🍲🍲🍲🍲🍲🍲🍲🍲Quire Guiso🍲🍲🍲🍲🍲🍲🍲🍲🍲\n
+        \n\n\nAutor: Alvaro el Curioso.\n\n\n\n\n\n
+🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚DESCUBRISTES EL HUEVO DE PASCUA🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚`)
+    n = 0
+    do {
+        console.error("🦠 🦠 🦠  El virus esta en tu conputadora\nCUIDADO < :() 🦠 🦠 🦠 ")
+        n++
+    } while (n != 999);
+    console.info("El antivirus fallo ni chatGPT te salvara 🦠 🦠 🦠 🦠 🦠 ")
+}
